591d4090b8371e8cf8198dc5f56a96b119ff2c38
Brandon Racich <brandon.racich@gmail.com>
2018-02-28 20:25:06 -0800

    Moved files and added template.html and css

---
+++ transfers.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css"/>
+    <link rel="stylesheet" href="css/HolyGrail.css"/>
+    <link rel="stylesheet" href="css/nav.css">
+    <title>CRABZ-View Account Information</title>
+  </head>
+  <body class="HolyGrail">
+    <header>
+      <nav id="headerNav" class="space-between">
+        <div>
+          <a href="">Home</a>
+          <a href="">Currency exchange</a>
+          <a href="">Transfer</a>
+          <a href="">Summary</a>
+          <a href="">Account</a>
+        </div>
+        <div>
+          <a class="rightanker" href="">Login</a>
+          <a class="rightanker" href="">Sign up</a>
+        </div>
+      </nav>
+    </header>
+    <div class="HolyGrail-body">
+      <main class="HolyGrail-content">
+      </main>
+      <nav class="HolyGrail-nav">
+      </nav>
+      <aside class="HolyGrail-ads">
+      </aside>
+    </div>
+    <footer>
+    </footer>
+  </body>
+</html>
