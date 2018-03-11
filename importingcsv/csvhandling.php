5aae60b2fc16012fd92fb8bf725b73479443e7cb
nawaf alsuhaibani <nawaf.99@live.com>
2018-03-10 22:36:22 -0800

    adding cdv importing but notice that the files are locally tested

---
+++ importingcsv/csvhandling.php
@@ -0,0 +1,47 @@
+<?php
+require_once ('../connection/config.php');
+$conn = mysqli_connect(DB_HOST, DBUSER, DBPASS, DBNAME);
+$error = mysqli_connect_error();
+if ($error != null)
+    {
+    $output = "<p>Unable to connect to database</p>" . $error;
+    exit($output);
+    }
+?>
+<?php
+if (isset($_POST['import']))
+    {
+    $filename = $_FILES["file1"]['tmp_name'];
+		$filetype = $_FILES["file1"]["type"];
+    if ($_FILES["file1"]["error"] > 0)
+        {
+        echo "error";
+        }
+    if ($_FILES["file1"]["size"] > 0)
+        {
+        $sql = "LOAD DATA LOCAL INFILE '$filename' INTO TABLE cvsfileimport FIELDS TERMINATED BY ',' ";
+        if ($conn->query($sql) === TRUE)
+            {
+            echo "new data inserted $filetype";
+            }
+          else
+            {
+            echo "error inserting the data" . "<br />" . $conn->error;
+            }
+        }
+    }
+function get_all_record()
+    {
+    global $conn;
+    $sql1 = "select * from cvsfileimport;";
+    $result = $conn->query($sql1);
+    if ($result->num_rows > 0)
+        {
+        echo "<table><tr><th>Date</th><th>amount$</th><th>catagory</th>";
+        while ($row = $result->fetch_assoc())
+            {
+            echo "<tr><td> " . $row['dateNtime'] . "</td><td>" . $row['amountCost'] . "</td><td>" . $row['paytype'] . "</td></tr>";
+            }
+        }
+    }
+?>
